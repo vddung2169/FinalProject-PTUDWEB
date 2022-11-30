40838d6719b03880883d24202eb7bc8be89d52a0
_april.inme <dung8ahxh@gmail.com>
2022-11-30 13:13:33 +0700

    Detail

--- assets/js/rangeSlider.js
+++ assets/js/rangeSlider.js
@@ -1,40 +1,40 @@
-var lowerSlider = document.querySelector('#lower'),
-    upperSlider = document.querySelector('#upper'),
-    lowerVal = parseInt(lowerSlider.value);
-upperVal = parseInt(upperSlider.value);
+// var lowerSlider = document.querySelector('#lower'),
+//     upperSlider = document.querySelector('#upper'),
+//     lowerVal = parseInt(lowerSlider.value);
+// upperVal = parseInt(upperSlider.value);
 
-upperSlider.oninput = function() {
-    lowerVal = parseInt(lowerSlider.value);
-    upperVal = parseInt(upperSlider.value);
+// upperSlider.oninput = function() {
+//     lowerVal = parseInt(lowerSlider.value);
+//     upperVal = parseInt(upperSlider.value);
 
-    if (upperVal < lowerVal + 4) {
-        lowerSlider.value = upperVal - 4;
+//     if (upperVal < lowerVal + 4) {
+//         lowerSlider.value = upperVal - 4;
 
-        if (lowerVal == lowerSlider.min) {
-            upperSlider.value = 4;
-        }
-    }
-};
+//         if (lowerVal == lowerSlider.min) {
+//             upperSlider.value = 4;
+//         }
+//     }
+// };
 
 
-lowerSlider.oninput = function() {
-    lowerVal = parseInt(lowerSlider.value);
-    upperVal = parseInt(upperSlider.value);
+// lowerSlider.oninput = function() {
+//     lowerVal = parseInt(lowerSlider.value);
+//     upperVal = parseInt(upperSlider.value);
 
-    if (lowerVal > upperVal - 4) {
-        upperSlider.value = lowerVal + 4;
+//     if (lowerVal > upperVal - 4) {
+//         upperSlider.value = lowerVal + 4;
 
-        if (upperVal == upperSlider.max) {
-            lowerSlider.value = parseInt(upperSlider.max) - 4;
-        }
+//         if (upperVal == upperSlider.max) {
+//             lowerSlider.value = parseInt(upperSlider.max) - 4;
+//         }
 
-    }
-};
+//     }
+// };
 
-var modalWrap = document.querySelector('.modal-wrap');
-var signUpBtn = document.querySelector('.btn-signup');
+// var modalWrap = document.querySelector('.modal-wrap');
+// var signUpBtn = document.querySelector('.btn-signup');
 
-function showModal() {
-    modalWrap.style.display = 'flex';
-}
-signUpBtn.addEventListener('click', showModal);
+// function showModal() {
+//     modalWrap.style.display = 'flex';
+// }
+// signUpBtn.addEventListener('click', showModal);
