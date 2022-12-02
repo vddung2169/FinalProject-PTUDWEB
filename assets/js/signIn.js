b5c39e07591ce45aeb60963d66732fff4bae343c
_april.inme <dung8ahxh@gmail.com>
2022-12-02 15:33:52 +0700

    Sign In Sgn Up

---
+++ assets/js/signIn.js
@@ -0,0 +1,6 @@
+// Sign up
+// const signUpBtn = $('.signup');
+// console.log(signUpBtn);
+// signUpBtn.onclick = function() {
+//     window.open('https://www.facebook.com/vuducdung2169/', '_blank');
+// }
