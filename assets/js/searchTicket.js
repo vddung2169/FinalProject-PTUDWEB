c29d797cf971ee6f87773b07a2078097b01ca1fe
thaichihien <79949235+thaichihien@users.noreply.github.com>
2022-12-04 19:49:51 +0700

    add responsive and update searchTicket

--- assets/js/searchTicket.js
+++ assets/js/searchTicket.js
@@ -4,7 +4,17 @@ const homePage = $('.home');
 
 const ticketPage = $('.availableTicket-container');
 
-searchTicketBtn.onclick = function() {
+const homeItem = document.querySelectorAll(".home__popular-item")
+
+function showListRoute(e) {
+    e.preventDefault()
     homePage.classList.add('disabled');
     ticketPage.classList.remove('disabled');
 }
+
+
+searchTicketBtn.onclick = showListRoute
+homeItem.forEach(e => {
+    e.onclick = showListRoute
+})
+
