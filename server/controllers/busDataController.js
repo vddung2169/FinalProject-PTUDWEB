5bb1bc97251bda7de8e6096db0361dbd9564fd00
Nizo <nguyenduynien11102002@gmail.com>
2022-12-24 00:56:25 +0700

    commit busDataController

--- server/controllers/busDataController.js
+++ server/controllers/busDataController.js
@@ -1,18 +1,21 @@
 const dataController = require('./getDataController')
 
 
-const renderBus = async (req,res) => {
+const renderBus = async(req, res) => {
     try {
         // Mục đích công việc này là đọc hiểu code getDataController.js
         // coi ví dụ bên busDataController_copy.js
-         // TODO : Lấy dữ liệu tỉnh thành và truyền vào index.hbs
+        // TODO : Lấy dữ liệu tỉnh thành và truyền vào index.hbs
         //const tinhthanh
 
+        const tinhthanh = await dataController.getAllTinhthanh()
+        res.render('index', { tinhthanh })
+            // TODO lấy dữ liệu tất cả chuyến xe và truyền vào bus.hbs
+        const chuyenxe = await dataController.getAllChuyenxe()
+        res.render('bus', { chuyenxe })
 
-        // TODO lấy dữ liệu tất cả chuyến xe và truyền vào bus.hbs
 
 
-        
 
     } catch (error) {
         console.log(error.message)
