135d9f98c4fe3dab6cdd4eb6665d3c0c804cba5c
thaichihien <79949235+thaichihien@users.noreply.github.com>
2022-12-24 13:41:23 +0700

    merge bus data controller

--- server/controllers/busDataController.js
+++ server/controllers/busDataController.js
@@ -3,19 +3,27 @@ const dataController = require('./getDataController')
 
 const renderBus = async(req, res) => {
     try {
-        // Mục đích công việc này là đọc hiểu code getDataController.js
-        // coi ví dụ bên busDataController_copy.js
-        // TODO : Lấy dữ liệu tỉnh thành và truyền vào index.hbs
-        //const tinhthanh
-
         const tinhthanh = await dataController.getAllTinhthanh()
-        res.render('index', { tinhthanh })
-            // TODO lấy dữ liệu tất cả chuyến xe và truyền vào bus.hbs
         const chuyenxe = await dataController.getAllChuyenxe()
-        res.render('bus', { chuyenxe })
 
+        res.render('bus', { chuyenxe,tinhthanh })
 
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json(error.message)
+    }
+}
 
+const renderIndex = async (req,res) => {
+    try {
+       
+       
+        const tinhthanh = await dataController.getAllTinhthanh()
+
+        const tuyenduongtop = await dataController.getAllTuyenduongtop()
+
+       
+        res.render('index',{tuyenduongtop,tinhthanh})
 
     } catch (error) {
         console.log(error.message)
@@ -27,6 +35,8 @@ const renderBus = async(req, res) => {
 
 
 
+
 module.exports = {
-    renderBus
+    renderBus,
+    renderIndex
 }
--- server/routes/userView.js
+++ server/routes/userView.js
@@ -1,12 +1,11 @@
 const router = require('express').Router()
 const busDataController = require('../controllers/busDataController')
-const busDataControllerCopy = require('../controllers/busDataController_copy')
 
 router.get('/login',(req,res) => res.render('login'))
 router.get('/register',(req,res) => res.render('register'))
 router.get('/history',(req,res) => res.render('history'))
 router.get('/bus',(req,res) => busDataController.renderBus(req,res))
-router.get('/',(req,res) => busDataControllerCopy.renderIndex(req,res))
+router.get('/',(req,res) => busDataController.renderIndex(req,res))
 
 
 
