85f949f5cb39e34c060233c372ce51e1d44fce79
_april.inme <dung8ahxh@gmail.com>
2022-12-25 10:46:11 +0700

    Change some FE

--- server/controllers/busDataController.js
+++ server/controllers/busDataController.js
@@ -5,20 +5,14 @@ const Tuyenduong = require('../database/models').Tuyenduong
 // Tham khảo cách inner join 2 table : https://sequelize.org/docs/v6/advanced-association-concepts/eager-loading/#required-eager-loading
 
 
-const getAllTuyenxe = (req,res) =>{
-
-
+const getAllTuyenxe = (req, res) => {
 
 }
 
-const getAllChuyenxe = (req,res) => {
+const getAllChuyenxe = (req, res) => {
 
 
 }
-
-
-
-
 
 
 module.exports = {
