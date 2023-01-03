051829108ec6f864454469622141292c13287899
thaichihien <79949235+thaichihien@users.noreply.github.com>
2023-01-03 09:25:59 +0700

    forget password (not test)

--- client/public/js/forgetpassword.js
+++ client/public/js/forgetpassword.js
@@ -4,6 +4,7 @@ const $$ = document.querySelectorAll.bind(document);
 const btnSend = $('.btn-send');
 const blockSendEmail = $('.send-email');
 const blockSendEmailSuccess = $('.send-email-success');
+const forgotForm = document.getElementById('forgotForm')
 
 btnSend.onclick = function() {
     blockSendEmail.classList.remove('active');
@@ -14,3 +15,34 @@ btnSend.onclick = function() {
         blockSendEmailSuccess.classList.remove('active');
     }
 }
+
+const forgotSubmit = async(e) => {
+    e.preventDefault()
+
+    try{
+        const payload = new URLSearchParams(new FormData(forgotForm))
+
+        const res = await fetch('/auth/forgot',
+            {
+                method : 'POST',
+                body : payload
+            }
+        )
+
+        const resJson = await res.json()
+
+        if(resJson === true){
+            blockSendEmail.classList.remove('active');
+            blockSendEmailSuccess.classList.add('active');
+        }
+        else{
+          // TODO SHOW ERROR MAIL
+            
+        }
+    } catch(error){
+        console.error(error.message)
+        
+    }
+}
+
+forgotForm.addEventListener('submit',forgotSubmit)
